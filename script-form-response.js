7d2450e07194901014a476b18b9b119c0843eadc
Rita <rrzorio@gmail.com>
2023-01-30 13:57:02 +0100

    creating and adding JS to forms

---
+++ script-form-response.js
@@ -0,0 +1,8 @@
+
+//Go-back butto takes you to index.html
+//selecting the button
+let button = document.getElementById('go-back');
+
+button.onclick = function(){
+    button.style.backgroundColor = 'yellow'
+};
